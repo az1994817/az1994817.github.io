52329ed0557efb97f9d3f03e2ba9dc74c8c5dccc
az1994817 <31766325+az1994817@users.noreply.github.com>
2017-12-01 09:06:40 +0800

    .
    。

--- ajax/index.php
+++ ajax/index.php
@@ -1,14 +1,14 @@
 <?php
-header(string:'content-type:text/html;charset=utf8');
+header('content-type:text/html;charset=utf8');
 $user=$_GET['user'];
 $pass=$_GET['pass'];
 
 if($user=='admin'){
     if($pass=='123456'){
-        echo "登陆成功<a href='main.html'>点击立即跳转</a>";
+        echo "登陆成功";
     }else{
-        echo "密码错误<a href='index.html'>点击立即返回</a>";
+        echo "密码错误";
     }
 }else{
-    echo "账号不存在!<a href='index.html'>点击立即返回</a>";
+    echo "账号不存在!";
 }
